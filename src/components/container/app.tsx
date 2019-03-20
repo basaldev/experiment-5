84abb77607292280f3b606c997d0caaf7a21bde7
Alex King <alexanderddking@gmail.com>
2019-03-20 19:29:55 +0900

    navbar bug

--- src/components/container/app.tsx
+++ src/components/container/app.tsx
@@ -67,7 +67,9 @@ export function App() {
       >
         {content}
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={12} className={css`{
+        ${currentPage().name  === 'LOGIN_PAGE' ? 'visibility:hidden;' : ''}
+      }`}>
         <Navbar
           value={currentPage().value}
           routes={[
